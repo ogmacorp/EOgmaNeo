2ed9f7c2eec0eb21f7a662b7d71735a5db9bce99
Eric Laukien <ericlaukien@gmail.com>
2018-07-19 12:19:40 -0400

    Different activation

--- source/eogmaneo/Layer.cpp
+++ source/eogmaneo/Layer.cpp
@@ -102,7 +102,7 @@ void Layer::columnForward(int ci) {
                         for (int c = 0; c < _columnSize; c++) {
                             int hiddenCellIndex = ci + c * _hiddenWidth * _hiddenHeight;
                             
-                            columnActivations[c] += _feedForwardWeights[v][hiddenCellIndex][wi] * std::max(0.0f, 1.0f - recon);
+                            columnActivations[c] += std::max(0.0f, _feedForwardWeights[v][hiddenCellIndex][wi] - recon);
                         }
                     }
                 }
