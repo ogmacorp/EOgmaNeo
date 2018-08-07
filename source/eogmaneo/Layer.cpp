9b2e188a2a35e71488a8087f1f3827252693c16b
Eric Laukien <ericlaukien@gmail.com>
2018-08-07 12:17:11 -0400

    Dec weights

--- source/eogmaneo/Layer.cpp
+++ source/eogmaneo/Layer.cpp
@@ -78,7 +78,7 @@ void Layer::columnForward(int ci) {
 
                             float target = (c == inputIndexPrev ? 1.0f : 0.0f);
 
-                            _feedForwardWeights[v][hiddenCellIndexPrev][wi] = std::max(0.0f, _feedForwardWeights[v][hiddenCellIndexPrev][wi] + _alpha * (target - recon));
+                            _feedForwardWeights[v][hiddenCellIndexPrev][wi] = std::max(0.0f, _feedForwardWeights[v][hiddenCellIndexPrev][wi] + _alpha * std::min(0.0f, target - recon));
                         }
                     }
 
