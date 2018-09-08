0903bbc26e6d37041e4c2fb51358a0a79728d843
Eric Laukien <ericlaukien@gmail.com>
2018-09-08 14:56:13 -0400

    On action

--- source/eogmaneo/Layer.h
+++ source/eogmaneo/Layer.h
@@ -104,7 +104,7 @@ namespace eogmaneo {
     struct HistorySample {
         std::vector<int> _hiddenStates;
         std::vector<int> _feedBack;
-        std::vector<std::vector<int> > _predictionsPrev;
+        std::vector<std::vector<int> > _inputs;
         float _reward;
     };
 
@@ -172,11 +172,6 @@ namespace eogmaneo {
         float _gamma;
 
         /*!
-        \brief Exploration rate.
-        */
-        float _epsilon;
-
-        /*!
         \brief Sparse coding iterations.
         */
         int _codeIters;
@@ -195,7 +190,7 @@ namespace eogmaneo {
         \brief Initialize defaults.
         */
         Layer()
-        : _alpha(0.1f), _beta(0.05f), _gamma(0.9f), _epsilon(0.01f), _codeIters(4), _maxHistorySamples(1024), _replayIters(16)
+        : _alpha(0.1f), _beta(0.05f), _gamma(0.9f), _codeIters(4), _maxHistorySamples(1024), _replayIters(16)
         {}
 
         /*!
